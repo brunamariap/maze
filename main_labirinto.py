331f777d42452a793614dffd675ee1052126d6c9
Bruna Maria <bruna.mariap@outlook.com>
2023-02-08 13:48:43 -0300

    Pequenos ajustes

--- main_labirinto.py
+++ main_labirinto.py
@@ -11,12 +11,12 @@ PERCORRIDO = '.'
 
 class Maze:
     def __init__(self):
-        self.map = None
+        self.__map = None
         self.entrada = None
         self.fim = None
         self.num_linhas = None
         self.num_colunas = None
-        self.pilha = Stack()
+        self.__stack = Stack()
 
     def run(self):
         self.__read_map()
@@ -25,22 +25,22 @@ class Maze:
 
     def __read_map(self):
         with open('labirinto.txt', 'r') as file:
-            self.map = file.read()
-        self.map = self.map.split('\n')
-        self.num_colunas = len(self.map[0])
-        self.num_linhas = len(self.map)
+            self.__map = file.read()
+        self.__map = self.__map.split('\n')
+        self.num_colunas = len(self.__map[0])
+        self.num_linhas = len(self.__map)
 
-        for i in self.map:
+        for i in self.__map:
             if len(i) != self.num_colunas:
                 raise ValueError(
                     'As linhas não pussuem o mesmo total de colunas')
 
         pos_mouse = None
         pos_saida = None
-        for i, linha in enumerate(self.map):
+        for i, row in enumerate(self.__map):
             if pos_mouse is None:
                 try:
-                    pos_mouse = linha.index(MOUSE)
+                    pos_mouse = row.index(MOUSE)
                     if pos_mouse:
                         self.entrada = (i, pos_mouse)
                 except:
@@ -49,19 +49,19 @@ class Maze:
 
             if pos_saida is None:
                 try:
-                    pos_saida = linha.index(SAIDA)
+                    pos_saida = row.index(SAIDA)
                     if pos_saida:
                         self.fim = (i, pos_saida)
                 except:
                     if i == self.num_linhas - 1:
                         raise ValueError("O mapa não possui saída")
 
-        self.pilha.push((self.entrada[0], self.entrada[1]))
+        self.__stack.push((self.entrada[0], self.entrada[1]))
         print('Entrada:', self.entrada)
         print('Saída:', self.fim)
 
     def __print_map(self):
-        for linha in self.map:
+        for linha in self.__map:
             print(linha)
         print('\n')
 
@@ -72,8 +72,8 @@ class Maze:
             return True
 
     def __encontrar_saida(self,):
-        top = self.pilha.peek()
-        print(self.pilha)
+        top = self.__stack.peek()
+        print(self.__stack)
 
         self.__draw_interface()
 
@@ -81,54 +81,54 @@ class Maze:
             return True
 
         # right
-        elif self.__position_valid(top[0], top[1] + 1) and self.map[top[0]][top[1] + 1] in (CORREDOR, SAIDA):
+        elif self.__position_valid(top[0], top[1] + 1) and self.__map[top[0]][top[1] + 1] in (CORREDOR, SAIDA):
             self.__parte_percorrida()
-            self.__mouse_current_position(0, 1)
+            self.__mouse_position(0, 1)
 
         # left
-        elif self.__position_valid(top[0], top[1] - 1) and self.map[top[0]][top[1] - 1] in (CORREDOR, SAIDA):
+        elif self.__position_valid(top[0], top[1] - 1) and self.__map[top[0]][top[1] - 1] in (CORREDOR, SAIDA):
             self.__parte_percorrida()
-            self.__mouse_current_position(0, -1)
+            self.__mouse_position(0, -1)
 
         # down
-        elif self.__position_valid(top[0] + 1, top[1]) and self.map[top[0] + 1][top[1]] in (CORREDOR, SAIDA):
+        elif self.__position_valid(top[0] + 1, top[1]) and self.__map[top[0] + 1][top[1]] in (CORREDOR, SAIDA):
             self.__parte_percorrida()
-            self.__mouse_current_position(1, 0)
+            self.__mouse_position(1, 0)
 
         # up
-        elif self.__position_valid(top[0] - 1, top[1]) and self.map[top[0] - 1][top[1]] in (CORREDOR, SAIDA):
+        elif self.__position_valid(top[0] - 1, top[1]) and self.__map[top[0] - 1][top[1]] in (CORREDOR, SAIDA):
             self.__parte_percorrida()
-            self.__mouse_current_position(-1, 0)
+            self.__mouse_position(-1, 0)
 
         else:
             self.__parte_percorrida()
-            self.pilha.pop()
+            self.__stack.pop()
 
-            if len(self.pilha) == 0:
+            if len(self.__stack) == 0:
                 raise ValueError('Não é possível encontrar a saída')
 
-            top = self.pilha.peek()
-            aux = list(self.map[top[0]])
+            top = self.__stack.peek()
+            aux = list(self.__map[top[0]])
             aux[top[1]] = MOUSE
             aux = ''.join(i for i in aux)
-            self.map[top[0]] = aux
+            self.__map[top[0]] = aux
 
         return False
 
     def __parte_percorrida(self):
-        top_p = self.pilha.peek()
-        aux = list(self.map[top_p[0]])
+        top_p = self.__stack.peek()
+        aux = list(self.__map[top_p[0]])
         aux[top_p[1]] = PERCORRIDO
         aux = ''.join(i for i in aux)
-        self.map[top_p[0]] = aux
+        self.__map[top_p[0]] = aux
 
-    def __mouse_current_position(self, x, y):
-        top = self.pilha.peek()
-        aux = list(self.map[top[0] + x])
+    def __mouse_position(self, x, y):
+        top = self.__stack.peek()
+        aux = list(self.__map[top[0] + x])
         aux[top[1] + y] = MOUSE
         aux = ''.join(i for i in aux)
-        self.map[top[0] + x] = aux
-        self.pilha.push((top[0] + x, top[1] + y))
+        self.__map[top[0] + x] = aux
+        self.__stack.push((top[0] + x, top[1] + y))
 
     def __draw_interface(self):
         self.master = Tk()
@@ -136,10 +136,8 @@ class Maze:
         self.dimensoes_quadrado = 64
         altura = self.num_linhas * self.dimensoes_quadrado
         largura = self.num_colunas * self.dimensoes_quadrado
-        mapa = self.map
+        mapa = self.__map
         self.master.geometry(f'{largura}x{altura}+317+34')
-
-        # self.master.wm_resizable()
 
         for i, linha in enumerate(mapa):
             for j, coluna in enumerate(mapa[i]):
